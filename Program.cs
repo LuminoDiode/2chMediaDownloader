33be270b0256d41d7869066e121b2a8e6853d754
Rodion Ryzhov <65biso2@gmail.com>
2021-04-25 00:34:42 +0300

    Release Condidate

--- Program.cs
+++ Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Net;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Windows.Forms;
 using System.Drawing;
-using System.Net.Cache;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
 namespace _2chMediaDownloader
@@ -27,112 +26,115 @@ namespace _2chMediaDownloader
 		static readonly DirectoryInfo SaveDirectoryInfo =
 			new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\2chDownloads");
 
+		const string Trc = @"Trace>>";
+		const string Wrn = @"Warning>>";
+		const string Err = @"Error>>";
+
 		[DllImport("kernel32")] static extern bool AllocConsole();
 		[STAThread]
 		public static void Main()
 		{
 			AllocConsole();
 			Trace.Listeners.Add(new ConsoleTraceListener());
-			Trace.WriteLine("Console initialized and listening Trace class.");
+			Trace.WriteLine(Trc + "Console initialized and listening Trace");
 
-			Trace.WriteLine("Creating UI instance...");
+			Trace.WriteLine(Trc + "Ensuring application folder is created in user personal");
+			SaveDirectoryInfo.Create();
+
+			Trace.WriteLine(Trc + "Creating UI instance");
 			UI = new DownloaderUI();
 
-			Trace.WriteLine("Binding download button...");
+			Trace.WriteLine(Trc + "Binding download button");
 			UI.DownloadButtonClick += DownloadButton_Click;
 
-			Trace.WriteLine("Running UI...");
+			Trace.WriteLine(Trc + "Running UI");
 			Application.Run((Form)UI);
+
 		}
+
 
 		public static void DownloadButton_Click(object sender, EventArgs ea)
 		{
-			Trace.WriteLine("Download button clicked.");
+			Trace.WriteLine(Trc + "Download button clicked");
 
-			Trace.WriteLine("Disabling UI...");
+			Trace.WriteLine(Trc + "Disabling UI");
 			((Form)(UI)).Enabled = false;
+
+			Trace.WriteLine(Trc + "Initializing WebClient with given usercode_auth cookie");
 			WebClient DownloaderWC = new WebClient();
+			if (!string.IsNullOrEmpty(UI.UserCodeAuth.Trim())) DownloaderWC.Headers.Add(HttpRequestHeader.Cookie, $"usercode_auth={UI.UserCodeAuth}");
+			else Trace.WriteLine(Wrn + "Unacceptable usercode_auth parameter was ignored");
+
 			HtmlDocument CurrentPage = new HtmlDocument();
 			IEnumerable<Uri> MediaUris;
 			DirectoryInfo CurrentDirectory;
 
-			Trace.WriteLine("Beginning recognizing links and download...");
+			Trace.WriteLine(Trc + "Beginning recognizing links");
 
 			foreach (Uri ThreadUriMatch in GetThreadURIsFromText(UI.UrisInput))
 			{
-				Trace.WriteLine($"Thread uri \"{ThreadUriMatch}\" recognized.");
+				Trace.WriteLine(Trc + $"Thread url \"{ThreadUriMatch}\" recognized and pending download");
 
 				try
 				{
 					CurrentPage.LoadHtml(DownloaderWC.DownloadString(ThreadUriMatch));
 				}
-				catch (WebException we)
-				{
-					Trace.WriteLine(
-						$"Web error while trying to access thread page at \"{ThreadUriMatch}\": \"{we.Message}\".");
-					continue;
-				}
 				catch (Exception e)
 				{
-					Trace.WriteLine(
-						$"Unexpected error while trying to access thread page at \"{ThreadUriMatch}\": \"{e.Message}\".");
+					Trace.WriteLine(Err + $"Exception while trying to access thread page at \"{ThreadUriMatch}\": \"{e.Message}\"");
 					continue;
 				}
 
-				Trace.WriteLine($"Thread page at \"{ThreadUriMatch}\" downloaded.");
+				Trace.WriteLine(Trc + $"Thread page at \"{ThreadUriMatch}\" downloaded and pending parsing");
 
-				Trace.WriteLine($"Parsing page at \"{ThreadUriMatch}\" to media links...");
 				try
 				{
 					MediaUris = GetMediaURIsFromThreadHtmlPage(CurrentPage);
 				}
 				catch (Exception e)
 				{
-					Trace.WriteLine(
-						$"Unexpected error while parsing page at \"{ThreadUriMatch}\" to media links: \"{e.Message}\".");
+					Trace.WriteLine(Err + $"Exception while parsing page at \"{ThreadUriMatch}\" to media links: \"{e.Message}\"");
 					continue;
 				}
 
-				Trace.WriteLine($"Total media links found at \"{ThreadUriMatch}\": {MediaUris.Count()}.");
-				Trace.WriteLine(
-					$"Media links found at \"{ThreadUriMatch}\": \"{string.Join(';', MediaUris.Select(x => '"' + x.AbsoluteUri + '"'))}\".");
+				Trace.WriteLine(Trc + $"Total media links found at \"{ThreadUriMatch}\": {MediaUris.Count()}.");
+				Trace.WriteLine(Trc + $"Media links found at \"{ThreadUriMatch}\": \"{string.Join(';', MediaUris)}\"");
 
-				Trace.WriteLine($"Beginning download files from thread at \"{ThreadUriMatch}\".");
+				Trace.WriteLine(Trc + $"Beginning download files from thread at \"{ThreadUriMatch}\".");
 
 				if (UI.SubFoldersRequired)
+				{
+					Trace.WriteLine(Trc + $"Subdirectory is required and created for thread at \"{ThreadUriMatch}\" and pending creating");
 					CurrentDirectory = SaveDirectoryInfo.CreateSubdirectory(GetThreadNumberFromURI(ThreadUriMatch));
+				}
 				else
+				{
+					Trace.WriteLine(Trc + $"Subdirectory is not required for thread at \"{ThreadUriMatch}\"");
 					CurrentDirectory = SaveDirectoryInfo;
-
-				CurrentDirectory.Create();
+				}
+				Trace.WriteLine(Trc + $"Directory at {CurrentDirectory.FullName} will be used for current thread medias");
 
 				foreach (Uri MediaUri in MediaUris)
 				{
-					FileInfo CurrentFile = new FileInfo(CurrentDirectory.FullName + GetFileNameFromURI(MediaUri));
-					if (CurrentFile.Exists)
+					FileInfo CurrentFile = new FileInfo(CurrentDirectory.FullName + '\\' + GetFileNameFromURI(MediaUri));
+
+					Trace.WriteLine(Trc + $"Pending download file at \"{MediaUri}\" from thread at \"{ThreadUriMatch}\" as \"{CurrentFile.FullName}\"");
+
+					if (CurrentFile.Exists && CurrentFile.Length > 1)
 					{
-						Trace.WriteLine($"File at \"{MediaUri}\" from thread at \"{ThreadUriMatch}\" skipped as existing at \"{CurrentFile.FullName}\".");
+						Trace.WriteLine(Trc + $"File at \"{MediaUri}\" from thread at \"{ThreadUriMatch}\" skipped as existing at \"{CurrentFile.FullName}\".");
+						continue;
 					}
 
 					try
 					{
-						Trace.WriteLine(
-							$"Beginning download file at \"{MediaUri}\" from thread at \"{ThreadUriMatch}\" as \"{CurrentFile.FullName}\".");
 						DownloaderWC.DownloadFile(MediaUri, CurrentFile.FullName);
-					}
-					catch (WebException we)
-					{
-						Trace.WriteLine(
-							$"Web error while trying to download file at \"{MediaUri}\" from thread at \"{ThreadUriMatch}\" as \"{CurrentFile.FullName}\": \"{we.Message}\".");
-						if (we.InnerException != null) Console.WriteLine(we.InnerException.Message);
-						continue;
 					}
 					catch (Exception e)
 					{
-						Trace.WriteLine(
-							$"Unexpected error while trying to download file at \"{MediaUri}\" from thread at \"{ThreadUriMatch}\" as \"{CurrentFile.FullName}\": \"{e.Message}\".");
+						Trace.WriteLine(Err + $"Exception while trying to download file at \"{MediaUri}\" from thread at \"{ThreadUriMatch}\" as \"{CurrentFile.FullName}\": \"{e.Message}\"");
 					}
-					Trace.WriteLine($"File at \"{MediaUri}\" from thread at \"{ThreadUriMatch}\" saved as \"{CurrentFile.FullName}\".");
+					Trace.WriteLine($"File at \"{MediaUri}\" from thread at \"{ThreadUriMatch}\" saved as \"{CurrentFile.FullName}\"");
 				}
 			}
 
@@ -160,20 +162,23 @@ namespace _2chMediaDownloader
 
 	class DownloaderUI
 	{
+		Label UsercodeAuthLabel = new Label { AutoSize = true, Text = "usercode_auth", Width = 100 };
+		TextBox UsercodeAuthTextBox = new TextBox { Text = @"" };
 		Label InfoLabel = new Label { Text = "Inset your 2ch.hk thread URIs here:" };
-		RichTextBox URIsTextBox = new RichTextBox();
+		RichTextBox URIsTextBox = new RichTextBox { };
 		CheckBox CreateSubFoldersCheckBox = new CheckBox { Text = "Create sub-folder for each thread", Checked = true };
 		Button DownloadButton = new Button { Text = "Start downloading" };
-		Form MainForm = new Form() { };
+		Form MainForm = new Form { };
 
 		public DownloaderUI()
 		{
-			Control[] AllControls = { InfoLabel, URIsTextBox, CreateSubFoldersCheckBox, DownloadButton };
+			Control[] AllControls = { Controls.ToHorizontalStackPanel(new Control[] { UsercodeAuthLabel, UsercodeAuthTextBox }), InfoLabel, URIsTextBox, CreateSubFoldersCheckBox, DownloadButton };
 			Controls.ToSameWidth(AllControls, 300);
 			Panel AllElementsPanel = Controls.ToVerticalStackPanel(AllControls);
 			MainForm.Controls.Add(AllElementsPanel);
 			MainForm.ClientSize = new Size(AllElementsPanel.Width, AllElementsPanel.Height);
 			MainForm.MinimumSize = MainForm.MaximumSize = MainForm.Size;
+			UsercodeAuthTextBox.Width = AllElementsPanel.ClientSize.Width - UsercodeAuthLabel.Width;
 		}
 
 		public static explicit operator Form(DownloaderUI dui)
@@ -184,6 +189,7 @@ namespace _2chMediaDownloader
 		public Form Form => MainForm;
 		public string UrisInput => URIsTextBox.Text;
 		public bool SubFoldersRequired => CreateSubFoldersCheckBox.Checked;
+		public string UserCodeAuth => UsercodeAuthTextBox.Text;
 
 		public event EventHandler DownloadButtonClick
 		{
@@ -194,7 +200,7 @@ namespace _2chMediaDownloader
 
 	class Controls
 	{
-		public static Panel ToVerticalStackPanel(IList<Control> Controls, int Space = 0)
+		public static Panel ToVerticalStackPanel(IList<Control> Controls)
 		{
 			Panel Out = new Panel();
 			int CurrentHeight = 0;
@@ -203,7 +209,7 @@ namespace _2chMediaDownloader
 			{
 				Controls[i].Location = new Point(0, CurrentHeight);
 				Out.Controls.Add(Controls[i]);
-				CurrentHeight += Controls[i].Height + Space;
+				CurrentHeight += Controls[i].Height;
 			}
 
 			Out.Height = Controls.Last().Location.Y + Controls.Last().Height;
@@ -212,6 +218,25 @@ namespace _2chMediaDownloader
 
 			return Out;
 		}
+		public static Panel ToHorizontalStackPanel(IList<Control> Controls)
+		{
+			Panel Out = new Panel();
+			int CurrentWidth = 0;
+
+			for (int i = 0; i < Controls.Count; i++)
+			{
+				Controls[i].Location = new Point(CurrentWidth, 0);
+				Out.Controls.Add(Controls[i]);
+				CurrentWidth += Controls[i].Width;
+			}
+
+			Out.Width = CurrentWidth;
+			Out.Height = Controls.Max(x => x.Height);
+			Out.BackColor = Color.Transparent;
+
+			return Out;
+		}
+
 
 		public static void ToSameWidth(IList<Control> Controls, int Width)
 		{
